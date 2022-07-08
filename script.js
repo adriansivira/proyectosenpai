f0230af213145cd846314910ce646f9d64f36b7c
Adrian Sivira <adriansivira@gmail.com>
2022-07-07 23:26:14 -0300

    proyecto final

--- script.js
+++ script.js
@@ -45,3 +45,47 @@ function contador() {
         }
     })
 }
+
+const navus = document.querySelector(".nosotros")
+const navnews = document.querySelector(".noticias")
+const navhelp = document.querySelector(".ayuda")
+const buttonav = document.querySelector(".button_nav")
+
+navus.addEventListener('mouseover', () => navus.style.backgroundColor = "#5227cc")
+navus.addEventListener('mouseout', () => navus.style = "")
+navnews.addEventListener('mouseover', () => navnews.style.backgroundColor = "#5227cc")
+navnews.addEventListener('mouseout', () => navnews.style = "")
+navhelp.addEventListener('mouseover', () => navhelp.style.backgroundColor = "#5227cc")
+navhelp.addEventListener('mouseout', () => navhelp.style = "")
+
+navus.addEventListener('mousedown', () => navus.style.backgroundColor = "#5327cc75")
+navus.addEventListener('mouseup', () => navus.style = "")
+navnews.addEventListener('mousedown', () => navnews.style.backgroundColor = "#5327cc75")
+navnews.addEventListener('mouseup', () => navnews.style = "")
+navhelp.addEventListener('mousedown', () => navhelp.style.backgroundColor = "#5327cc75")
+navhelp.addEventListener('mouseup', () => navhelp.style = "")
+
+buttonav.addEventListener('mousedown', () => buttonav.style.backgroundColor = "#5327cc75")
+buttonav.addEventListener('mouseup', () => buttonav.style = "")
+buttonav.addEventListener('mouseover', () => buttonav.style.boxShadow = "4px 5px 10px black")
+buttonav.addEventListener('mouseout', () => buttonav.style.boxShadow = "")
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
